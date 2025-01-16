99e3f8ff0136afbe80e16c7504ce9b83c7f4acd1
fairouzijerdaoun205 <fairouz.ijerdaoun@univ-tlse3.fr>
2025-01-16 11:42:57 +0100

    Ajout de la méthode parler dans la classe Gaulois et test dans TestGaulois

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -1,5 +1,28 @@
 package personnages;
 
 public class Gaulois {
+	 private String nom;
+	    private int force;
 
-}
+	   
+	    public Gaulois(String nom, int force) {
+	        this.nom = nom;
+	        this.force = force;
+	    }
+       public String getNom() {
+    	   return nom;
+       }
+       public void parler(String texte) {
+           System.out.println("Le gaulois " + nom + " : « " + texte + " ».");
+       }
+       public static void main(String[] args) {
+         
+           Gaulois asterix = new Gaulois("Astérix", 8);
+
+          
+           asterix.parler("Bonjour à tous");
+
+           
+           System.out.println(asterix); 
+       }
+   }
---
+++ src/personnages/TestPersonnages.java
@@ -0,0 +1,13 @@
+package personnages;
+
+public class TestPersonnages {
+	 public static void main(String[] args) {
+	        
+	        Gaulois asterix = new Gaulois("Astérix", 8);
+
+	      
+	        System.out.println(asterix); 
+	    }
+	}
+
+
